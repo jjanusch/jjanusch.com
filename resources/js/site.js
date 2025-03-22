be7f5144c520d8da7031fe8308a2193566df9f04
Josh Janusch <josh.janusch@vincit.com>
2025-03-22 13:25:45 -0700

    Properly include FontAwesome for icons (was previously loading it manually)

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,11 +1,13 @@
-// Using ES6 import syntax
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import html from 'highlight.js/lib/languages/xml';
 import css from 'highlight.js/lib/languages/css';
 import php from 'highlight.js/lib/languages/php';
-
 import 'highlight.js/styles/atom-one-dark.css';
+
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
+import '@fortawesome/fontawesome-free/css/brands.min.css';
 
 // Then register the languages you need
 hljs.registerLanguage('javascript', javascript);
